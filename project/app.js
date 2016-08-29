b9dd737cff99a85f26414b481a7c0e6020f92768
Joseph Lee <jin3lee@gmail.com>
2016-08-29 04:09:59 -0700

    added sockets to send data to client from server of user's position and number

--- project/app.js
+++ project/app.js
@@ -7,4 +7,37 @@ app.get('/', function(req, res) {
 })
 app.use('/client', express.static('/Users/josephlee/Desktop/rainingChainGuide/project/client'));
 
-serv.listen(2000);
+console.log("Server started."); // messages the server when server started
+serv.listen(2000);  // listening to port 2000 on localhost (localhost:2000)
+
+var io = require('socket.io') (serv, {});   
+
+var SOCKET_LIST = {};
+
+io.sockets.on('connection', function(socket){
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
+    socket.number = "" + Math.floor(10 * Math.random());
+    SOCKET_LIST[socket.id] = socket;
+	socket.on('disconnect', function(){
+        delete SOCKET_LIST[socket.id];
+    })
+});
+
+setInterval(function(){
+    var pack = [];
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.x++;
+        socket.y++;
+        pack.push({
+            x:socket.x, 
+            y:socket.y,
+            number:socket.number
+        });
+    }
+    for(var i in SOCKET_LIST){
+        socket.emit('newPosition', pack);
+    }
+}, 1000/25);
